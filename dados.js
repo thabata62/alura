a614dbaf17b62e1a38ee9014aa8ee2e2f32e78a2
thabata62 <123984601+thabata62@users.noreply.github.com>
2024-09-06 20:26:26 -0400

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+
+    let dados = [
+        {
+          titulo: "Variáveis",
+          descricao: "é como um recipiente que armazena um valor, que pode ser um número, texto, objeto, etc. Esse valor pode ser modificado durante a execução do programa.",
+          link: "https://www.devmedia.com.br/javascript-variaveis-e-constantes/41012",
+          tags:"vari, variaveis, variavel"
+        },
+        {
+          titulo: "Objetos em JavaScript",
+          descricao: "São estruturas de dados que permitem agrupar propriedades e métodos. São como coleções de chave-valor.",
+          link: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Working_with_objects",
+          tags: "objeto, js, java, script"
+        },
+        {
+          titulo: "Tipos de Dados em JavaScript",
+          descricao: "JavaScript possui diversos tipos de dados, como números, strings, booleanos, arrays, objetos, etc.",
+          link: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Data_structures",
+          tags: "dados, dado, javas, scrip"
+        }
+      
+
+
+];    
